5314ccaf91ffeda4ce3f8a126b6f5aa716cb2f80
Snow Aye <saungaye7@gmail.com>
2019-11-09 10:43:17 +0630

    Fix wrong declaration error in BaseActivity

--- app/src/main/java/com/padc/batch9/assignment11/activity/BaseActivity.kt
+++ app/src/main/java/com/padc/batch9/assignment11/activity/BaseActivity.kt
@@ -2,12 +2,14 @@ package com.padc.padckotlin.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.padc.batch9.assignment11.data.model.AppDataModel
+import com.padc.batch9.assignment11.data.model.AppDataModelImpl
 
 abstract class BaseActivity: AppCompatActivity() {
-    protected lateinit var model: PhotoDataModel
+    protected lateinit var model: AppDataModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        model = PhotoDataModelImpl
+        model = AppDataModelImpl
     }
 }
